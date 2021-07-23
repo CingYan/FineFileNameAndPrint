721866a85c01b9c24e6370c3228cb3223d3d2818
CingYan <gemo0002@hotmail.com>
2021-07-24 01:19:46 +0800

    Done

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,50 @@ Partial Class Form1
     '請勿使用程式碼編輯器進行修改。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.T1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.t2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'T1
+        '
+        Me.T1.Location = New System.Drawing.Point(12, 12)
+        Me.T1.Name = "T1"
+        Me.T1.Size = New System.Drawing.Size(100, 22)
+        Me.T1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(21, 69)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "NO"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        't2
+        '
+        Me.t2.Location = New System.Drawing.Point(12, 41)
+        Me.t2.Name = "t2"
+        Me.t2.Size = New System.Drawing.Size(100, 22)
+        Me.t2.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(120, 97)
+        Me.Controls.Add(Me.t2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.T1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents T1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents t2 As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,18 @@
-﻿Public Class Form1
-
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FoundFilePath As String = T1.Text
+        Dim textwrite As String = t2.Text
+        Dim DirInfo As IO.DirectoryInfo
+        Dim short1, fuckyoujustprint
+        DirInfo = New IO.DirectoryInfo(FoundFilePath)
+        For Each file In DirInfo.GetFiles("*.xml", IO.SearchOption.AllDirectories)
+            short1 = Split(file.Name, ".img.xml")
+            fuckyoujustprint = Strings.Right(short1(0), 7)
+            fuckyoujustprint = fuckyoujustprint & vbCrLf
+            My.Computer.FileSystem.WriteAllText(
+                textwrite, fuckyoujustprint, True)
+        Next file
+        MsgBox("done")
+    End Sub
 End Class
